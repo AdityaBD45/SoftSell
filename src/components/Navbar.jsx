e4442de454fce88cfb841b4a7e73114852f798fb
Aditya B D <lalitaadityadharaneppanavar@gmail.com>
2025-05-11 14:07:08 +0530

    favicon adding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/logo.png'; // Update path if needed
+import logo from '../assets/favicon.png'; // Update path if needed
 
 const Navbar = ({ isDarkMode, toggleDarkMode }) => {
   const scrollToTop = () => {
